96e07b916acc9262e94216e7ff4eeb0338bae10d
badcast <70087152+badcast@users.noreply.github.com>
2022-08-06 13:56:19 +0600

    update ronine-engine script added

---
+++ update-engine.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#this script for update RoninEngine to latest version
+
+echo "Adding remote ronin-engine -> engine"
+git remote add -f engine https://github.com/badcast/ronin-engine.git
+
+git subtree pull --prefix=RoninEngine/ engine master
+git pull -v engine master

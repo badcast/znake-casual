5f8fc65a24251cbcfaff30812149e516b5639c6a
badcast <70087152+badcast@users.noreply.github.com>
2022-08-06 17:39:26 +0600

    not-x

--- update-engine.sh
+++ update-engine.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 #this script for update RoninEngine to latest version
-
 echo "Adding remote ronin-engine -> engine"
 git remote add -f engine https://github.com/badcast/ronin-engine.git
 
+echo "removing existence sources"
+rm -rf ./RoninEngine
+
+git subtree add --prefix=RoninEngine https://github.com/badcast/ronin-engine.git master
+
+
 git fetch engine
+mkdir -p ./RoninEngine
 git subtree pull --prefix=RoninEngine/ engine master
 git pull -v engine master
 

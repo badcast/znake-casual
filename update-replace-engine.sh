84a52f634ef1d5ef63dfd1d9f86d0ed43b5319a5
badcast <70087152+badcast@users.noreply.github.com>
2022-08-06 17:42:38 +0600

    updated ronin-engine

--- update-replace-engine.sh
+++ update-replace-engine.sh
@@ -4,6 +4,12 @@
 echo "Adding remote ronin-engine -> engine"
 git remote add -f engine https://github.com/badcast/ronin-engine.git
 
+echo "removing existence sources"
+rm -rf ./RoninEngine
+
+git subtree add --prefix=RoninEngine https://github.com/badcast/ronin-engine.git master
+
+mkdir -p ./RoninEngine
 git subtree pull --prefix=RoninEngine/ engine master
 git pull -v engine master
 
